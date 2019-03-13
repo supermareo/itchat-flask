9463845ba0b527e5bf3b1e730fe8325276deb659
superychen <superychen@gmail.com>
2019-03-14 02:12:51 +0800

    尝试继集æ照片墙

--- web/static/js/wechat.js
+++ web/static/js/wechat.js
@@ -53,6 +53,7 @@ function changeTab(iid, did) {
     $('#' + did).addClass('on');
 }
 
+//加载性别分布表
 function load_gender() {
     // 基于准备好的dom，初始化echarts实例
     var myChart = echarts.init(document.getElementById('ec-gender'));
@@ -132,6 +133,36 @@ function load_gender() {
     myChart.setOption(option);
 }
 
+//加载好友头像照片墙
+function load_avatars() {
+    load_images([
+        {src: '46eae50db6.jpg', color: '#FFF', title: '测试图片1', url: ''},
+        {src: '661a66329a.jpg'},
+        {src: 'a8008ae7f7.jpg'},
+        {src: '69833ed09a.jpg'},
+        {src: 'fe00425d0a.jpg'},
+        {src: 'b05b0c6962.jpg'},
+        {src: '6a884d6ef2.jpg'},
+        {src: 'ed57bce8c4.jpg'},
+        {src: '94da4fb185.jpg'},
+        {src: 'd04505fe96.jpg'},
+        {src: '774573728f.jpg'},
+        {src: 'b05c9edc7f.jpg'},
+        {src: '8fe86226ff.jpg'},
+        {src: '7b4cc906b3.jpg'},
+        {src: 'f22e53da3a.jpg'},
+        {src: 'c136720da3.jpg'},
+        {src: 'd57dc05c81.jpg'},
+        {src: '65f8787807.jpg'},
+        {src: '200ff06c0e.jpg'},
+        {src: '5b0b0c8119.jpg'},
+        {src: '0e85d4fe6d.jpg'},
+        {src: '1ef7ea6559.jpg'},
+        {src: 'ed37a0925e.jpg'}
+    ])
+}
+
+//加载地域分布表
 function load_locations() {
     // 基于准备好的dom，初始化echarts实例
     var myChart = echarts.init(document.getElementById('ec-locations'));
@@ -186,10 +217,6 @@ function load_locations() {
 
     // 使用刚指定的配置项和数据显示图表。
     myChart.setOption(option);
-}
-
-function load_photos() {
-
 }
 
 // function load_locations() {
